938f312a9cef9710e70bd36db9f02f030e1c8eab
BruceTheGooose <30399240+BruceTheGooose@users.noreply.github.com>
2017-09-10 13:09:03 +1000

    Add files via upload

--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,15 @@
 ##Import files
-from django.conf.urls import url
-from home import views
-
+from django.conf.urls import url, include
+from . import views
+from django.contrib.auth.views import login, logout
 ##Patterns
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
+    #url(r'^login/$', login, {'template_name': 'personal/login.html'}),
     url(r'templates/personal/login.html$', views.login, name = 'login'),
     url(r'templates/personal/register.html$', views.register, name = 'register'),
+    url(r'^logout/$', logout, {'template_name': 'personal/logout.html'}),
+    #url(r'^register/$', views.register, name='register'),
     url(r'^map.html$', views.map, name = 'map'),
     url(r'templates/personal/profile.html$', views.profile, name = 'profile'),
     url(r'templates/personal/admin.html$', views.admin, name = 'admin'),
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from home.forms import RegistrationForm
 
 # Create your views here.
 
@@ -10,7 +11,20 @@ def login(request):
     return render(request, 'personal/login.html')
 
 def register(request):
-    return render(request, 'personal/register.html')
+    #if data is posted (from user submission) perform this
+    if request.method =='POST':
+        #form reads user input
+        form = RegistrationForm(request.POST)
+        #if the form contains valid data perform this
+        if form.is_valid():
+            #save information and redirect to specified location
+            form.save()
+            return redirect('/home')
+    #if request method is 'get' generate empty form
+    else:
+        form = RegistrationForm()
+        args = {'form': form}
+        return render(request, 'personal/register.html', args)
 
 def map(request):
     return render(request, 'personal/map.html')
